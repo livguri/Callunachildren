78c6fa9a46c32e338a622acf38ccafbb1c34933d
Liv Guri Velle <liv.guri.velle@moreforsk.no>
2019-10-16 20:47:35 +0200

    Reordered sites

--- calluna.R
+++ calluna.R
@@ -3,3 +3,8 @@ library(tidyverse)
 
 #load data
 calluna <- readxl::read_excel("Data/Feltskjema Fureneset 25.09.2019.xlsx", sheet = "målingerSept")
+
+calluna <- calluna %>% mutate(`Site name`= factor(`Site name`,levels = c("TR", "NO", "TA", "SM", "AU", "TV", "LY", "HI")))
+
+ggplot(calluna, aes(x = `Site name`, y = `Max hight`)) + 
+  geom_boxplot(notch = TRUE)
