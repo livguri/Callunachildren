37129dc3c9d1b82b01258643fc3a0ebcf25d9114
livguri <liv.guri.velle@moreforsk.no>
2019-10-17 13:13:56 +0200

    imported all data. joint together.

--- calluna.R
+++ calluna.R
@@ -1,13 +1,50 @@
 #load packages
 library(tidyverse)
+library(lme4)
 
 #load data
-calluna <- readxl::read_excel("Data/Feltskjema Fureneset 25.09.2019.xlsx", sheet = "målingerSept")
+Biomass <- readxl::read_excel("Data/Biomass.xlsx", sheet = "Biomass")
 
-calluna <- calluna %>% mutate(`Site name`= factor(`Site name`,levels = c("TR", "NO", "TA", "SM", "AU", "TV", "LY", "HI")))
+GreenGarden0 <- readxl::read_excel("Data/GreenGarden.xlsx", sheet = "GreenGarden")
 
-ggplot(calluna, aes(x = `Site name`, y = `Max hight`)) + 
+Greenhouse <- readxl::read_excel("Data/Greenhouse.xlsx", sheet = "Greenhouse") %>%
+  mutate(Site = toupper(Site))
+
+#check IDs
+
+Biomass %>% count(Site)
+Greenhouse %>% count(Site)
+GreenGarden0 %>% count(Site)
+GreenGarden0 %>% filter(str_detect(IdNr, "\\D"))
+
+GreenGarden <- GreenGarden0 %>% 
+  mutate(
+    IdNr = as.numeric(IdNr),
+    IdMum = str_remove(Block, "[A-C]"),
+    IdMum = as.numeric(IdMum), 
+    Block = str_remove(Block, "\\d+"))
+
+Biomass %>% anti_join(Greenhouse, by = c("IdNr"))
+Biomass %>% anti_join(Greenhouse, by = c("IdNr", "Site", "Block", "IdMum"))
+
+GreenGarden %>% anti_join(Greenhouse, by = c("IdNr"))
+GreenGarden %>% anti_join(Greenhouse, by = c("IdNr", "Site", "Block", "IdMum"))
+
+# joining datasets
+calluna <- Greenhouse %>% 
+  left_join(Biomass, by = c("IdNr", "Site", "Block", "IdMum")) %>% 
+  left_join(GreenGarden, by = c("IdNr", "Site", "Block", "IdMum")) %>% 
+  mutate(supermum=paste(Site,Block,IdMum))
+
+
+
+calluna %>% filter(!is.na(`Max hight`)) %>% count(Site,Block,IdMum) %>% arrange(n)
+
+calluna <- calluna %>% mutate(Site= factor(Site,levels = c("TR", "NO", "TA", "SM", "AU", "TV", "LY", "HI")))
+
+ggplot(calluna, aes(x = Site, y = `Kar8`)) + 
   geom_boxplot(notch = TRUE)
 
-hightmod <- lm(`Max hight` ~ `Site name`, data = calluna)
+hightmod <- lmer(`Max hight` ~ Site + (1|supermum), data = calluna)
 summary(hightmod)
+anova(hightmod, )
