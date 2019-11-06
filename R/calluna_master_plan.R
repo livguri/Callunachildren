45954b2de08eacfc6737c8c0b2be4c8f5e265273
richardjtelford <richard.telford@bio.uib.no>
2019-11-06 14:16:03 +0100

    site map

--- R/calluna_master_plan.R
+++ R/calluna_master_plan.R
@@ -3,14 +3,16 @@ library("tidyverse")
 library("drake")
 library("lme4")
 library("here")
+library("mapdata")
 
 #### import plans ####
 source(here("R", "import_data.R"))
-
+source(here("R", "make_plots.R"))
 
 #### combine plans ####
 calluna_plan <- bind_rows(
-  import_plan
+  import_plan,
+  plot_plan
   )
 
 ####configure plan####
---
+++ R/make_plots.R
@@ -0,0 +1,19 @@
+## make plots
+
+plot_plan <- drake_plan(
+  #site map
+  site_map = {
+    mp <- map_data("world", xlim = c(0, 30), ylim = c(55, 80))
+    
+    ggplot(meta, aes(x = Long, y = Lat, label = Site)) +
+      geom_map(map = mp, data = mp, aes(map_id = region),
+              inherit.aes = FALSE, fill = "grey70", colour = "grey60") +
+      geom_point() +
+      geom_text(hjust = 1.2) +
+      coord_quickmap() +
+      scale_x_continuous(expand = c(0.2, 0)) +
+      scale_y_continuous(expand = c(0.1, 0)) +
+      labs(x = "°E", y = "°N") +
+      theme_bw()
+  }
+)
